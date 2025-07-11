8d0a6ae2b3ee9fe39fa3507ac7b20a0130d70071
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-07-11 13:49:20 +0200

    rm ml stuff

--- shells/py.nix
+++ shells/py.nix
@@ -3,15 +3,20 @@ pkgs.mkShell {
   name = "python";
   packages = [
     (pkgs.python3.withPackages (p:
-      with p; [
-        keras
-        numpy
-        opencv-python
-        pandas
-        pillow
-        scikit-learn
-        tensorflow
-        torchaudio
+      with p;
+      [
+        # ML
+        # keras
+        # numpy
+        # opencv-python
+        # pandas
+        # pillow
+        # scikit-learn
+        # tensorflow
+        # torchaudio
+
+        # Rust
+        maturin
       ]))
   ];
 }
