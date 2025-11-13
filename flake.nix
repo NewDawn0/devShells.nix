cdb76f5d164ce201afccd9e7b9ec4b73b4b58a2f
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-11-13 18:56:16 +0100

    update nix-utils

--- flake.nix
+++ flake.nix
@@ -1,47 +1,39 @@
 {
-  description = "Development shells for nix";
+  description = "Default flake template";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nix-utils = {
+    utils = {
       url = "github:NewDawn0/nixUtils";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
     };
   };
-  outputs = {
-    self,
-    nix-utils,
-    ...
-  }: let
-    perSystem = nix-utils.lib.eachSystem {};
-  in {
-    checks = perSystem (
-      pkgs: _: {
-        deadnix = pkgs.runCommand "deadnix" {
-          nativeBuildInputs = [pkgs.deadnix];
-        } "deadnix --fail ${./.} && touch $out";
-      }
-    );
-    formatter = perSystem (pkgs: _: pkgs.alejandra);
-    overlays.default = _: prev: {
-      YOUR-PACKAGE = self.packages.${prev.system}.default;
+  outputs =
+    {
+      self,
+      utils,
+      ...
+    }:
+    {
+      checks = utils.lib.eachSystem { } (
+        p: with p; {
+          deadnix = pkgs.runCommand "deadnix" {
+            nativeBuildInputs = [ pkgs.deadnix ];
+          } "deadnix --fail ${./.} && touch $out";
+        }
+      );
+      formatter = utils.lib.eachSystem { } (p: p.pkgs.alejandra);
+      overlays.default = _: prev: {
+        YOUR-PACKAGE = self.packages.${prev.system}.default;
+      };
+      devShells = utils.lib.eachSystem { } (
+        p:
+        let
+          shells = with builtins; attrNames (readDir ./shells);
+          name = p.pkgs.lib.removeSuffix ".nix";
+          pkg = x: p.pkgs.callPackage ./shells/${x} { };
+        in
+        builtins.foldl' (xs: x: xs // { "${name x}" = pkg x; }) { } shells
+      );
     };
-    devShells = perSystem (
-      pkgs: _: let
-        inherit (pkgs.lib) removeSuffix;
-        inherit (pkgs.lib.attrsets) mergeAttrsList;
-        inherit (builtins) attrNames readDir;
-        name = removeSuffix ".nix";
-        shell = e: pkgs.callPackage (./shells + "/${e}") {};
-      in
-        mergeAttrsList (map (e: {"${name e}" = shell e;}) (attrNames (readDir ./shells)))
-    );
-    packages = perSystem (
-      pkgs: _: {
-        default = pkgs.hello;
-      }
-    );
-  };
 }
