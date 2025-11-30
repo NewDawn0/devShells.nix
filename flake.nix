b6e55b1998ecc3c5bd4317680d55b72fa09b93c8
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-11-30 23:31:50 +0100

    update flake to 25.11

--- flake.nix
+++ flake.nix
@@ -2,39 +2,37 @@
   description = "On the fly development environments";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
-    utils = {
-      url = "github:NewDawn0/nixUtils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    utils.url = "github:NewDawn0/nixUtils";
   };
-  outputs = {
-    self,
-    utils,
-    ...
-  }: {
-    checks = utils.lib.eachSystem {} (
-      p:
-        with p; {
+  outputs =
+    {
+      self,
+      utils,
+      ...
+    }:
+    {
+      checks = utils.lib.eachSystem { } (
+        p: with p; {
           deadnix = pkgs.runCommand "deadnix" {
-            nativeBuildInputs = [pkgs.deadnix];
+            nativeBuildInputs = [ pkgs.deadnix ];
           } "deadnix --fail ${./.} && touch $out";
           typos = pkgs.runCommand "typos" {
-            nativeBuildInputs = [pkgs.typos];
+            nativeBuildInputs = [ pkgs.typos ];
           } "typos --format brief && touch $out";
         }
-    );
-    formatter = utils.lib.eachSystem {} (p: p.pkgs.alejandra);
-    overlays.default = _: prev: {
-      YOUR-PACKAGE = self.packages.${prev.system}.default;
+      );
+      formatter = utils.lib.eachSystem { } (p: p.pkgs.alejandra);
+      overlays.default = _: prev: {
+        YOUR-PACKAGE = self.packages.${prev.system}.default;
+      };
+      devShells = utils.lib.eachSystem { } (
+        p:
+        let
+          shells = with builtins; attrNames (readDir ./shells);
+          name = p.pkgs.lib.removeSuffix ".nix";
+          pkg = x: p.pkgs.callPackage ./shells/${x} { };
+        in
+        builtins.foldl' (xs: x: xs // { "${name x}" = pkg x; }) { } shells
+      );
     };
-    devShells = utils.lib.eachSystem {} (
-      p: let
-        shells = with builtins; attrNames (readDir ./shells);
-        name = p.pkgs.lib.removeSuffix ".nix";
-        pkg = x: p.pkgs.callPackage ./shells/${x} {};
-      in
-        builtins.foldl' (xs: x: xs // {"${name x}" = pkg x;}) {} shells
-    );
-  };
 }
